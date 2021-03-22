d39a9e785a4a1eca49b3fb9a04c81e936faa3273
ilham <ilhammuhamads6999@gmail.com>
2021-03-22 14:39:58 +0700

    fix responsive

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -1,25 +1,32 @@
+// nav
+window.addEventListener("scroll", function() {
+	var nav = document.querySelector("nav");
+	nav.classList.toggle("navAnimate", window.scrollY > 50);
+})
+
+// carousel
 jQuery(document).ready(function($) {
-        		"use strict";
-        		//  TESTIMONIALS CAROUSEL HOOK
-		        $('#customers-testimonials').owlCarousel({
-		            loop: true,
-		            center: true,
-		            items: 3,
-		            margin: 0,
-		            autoplay: true,
-		            dots:true,
-		            autoplayTimeout: 8500,
-		            smartSpeed: 450,
-		            responsive: {
-		              0: {
-		                items: 1
-		              },
-		              768: {
-		                items: 2
-		              },
-		              1170: {
-		                items: 3
-		              }
-		            }
-		        });
-        	});
+	"use strict";
+	//  TESTIMONIALS CAROUSEL HOOK
+	$('#customers-testimonials').owlCarousel({
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 0,
+		autoplay: true,
+		dots: true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1170: {
+				items: 3
+			}
+		}
+	});
+});
